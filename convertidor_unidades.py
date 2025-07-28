14787e5f6ba3f234dbd258fe9945a1eb6070821e
ElTomi2 <61668231+ElTomi2@users.noreply.github.com>
2025-07-27 23:57:47 -0300

    Update convertidor_unidades.py

--- convertidor_unidades.py
+++ convertidor_unidades.py
@@ -1,19 +1,45 @@
-#Falta implementar unidades de distancia
+validos = (
+    "Celcius",
+    "Fahrenheit",
+    "Kelvin",
+    "Kilometros",
+    "Centimetros",
+    "Metros",
+    "Millas",
+    "Pies",
+    "Pulgadas")
+
 print("Convertidor de Unidades\n")
 outvalue = None
-inunit = input("Cuál es la unidad de entrada? (Celcius o Fahrenheit, Kelvin; Metros, Pies o Millas)\n").title()
-while inunit != "Celcius" and inunit != "Fahrenheit" and inunit != "Kelvin":
-    inunit = input("Ingrese una unidad válida\n")
+inunit = input("""Cuál es la unidad de entrada?
+Distancia:
+    Metros
+    Centimetros
+    Kilometros
+    Pulgadas
+    Pies
+    Millas
+Temperatura:
+    Celcius
+    Fahrenheit
+    Kelvin\n""").title().strip()
+
+while inunit not in validos:
+    inunit = input("Ingrese una unidad válida\n").title().strip()
+
 while True:
     try:
         inval = float(input("Y el valor?\n"))
         break
     except ValueError:
         print("Ingrese un valor válido")
-if inunit == "Celcius" or inunit == "Fahrenheit" or inunit == "Kelvin":
+        
+outvalue = inval
+
+if inunit in validos[0:3]:
     tipo_unit = "Temp"
-    outunit = input("A que unidad convertir?\n").title()
-    while outunit != "Celcius" and outunit != "Fahrenheit" and outunit != "Kelvin":
+    outunit = input("A que unidad convertir?\n").title().strip()
+    while outunit not in validos[0:3]:
         outunit = input("Ingrese una unidad de temperatura válida. ")
     match outunit:
         case "Celcius":
@@ -21,23 +47,99 @@ if inunit == "Celcius" or inunit == "Fahrenheit" or inunit == "Kelvin":
                 outvalue = (inval-32)*5/9
             elif inunit == "Kelvin":
                 outvalue = inval - 273.15
-            else:
-                outvalue = inval
         case "Fahrenheit":
             if inunit == "Celcius":
                 outvalue = (inval*9/5)+32
             elif inunit == "Kelvin":
                 outvalue = ((inval-273.15)*9/5)+32
-            else:
-                outvalue = inval
         case "Kelvin":
             if inunit == "Celcius":
                 outvalue = inval + 273.15
             elif inunit == "Fahrenheit":
                 outvalue = (inval -32)*5/9 + 273.15
-            else:
-                outvalue = inval
+elif inunit in validos[3:9]:
+    tipo_unit = "Dist"
+    outunit = input("A que unidad convertir?\n").title().strip()
+    while outunit not in validos[3:9]:
+        outunit = input("Ingrese una unidad de distancia válida. ")
+    match outunit:
+        case "Kilometros":
+            match inunit:
+                case "Metros":
+                    outvalue = inval*1000
+                case "Centimetros":
+                    outvalue = inval*100000
+                case "Millas":
+                    outvalue = inval/1.609
+                case "Pies":
+                    outvalue = inval*3280
+                case "Pulgadas":
+                    outvalue = inval*39370
+        case "Centimetros":
+            match inunit:
+                case "Kilometros":
+                    outvalue = inval/100000
+                case "Metros":
+                    outvalue = inval/100
+                case "Millas":
+                    outvalue = inval/1609
+                case "Pies":
+                    outvalue = inval/30.48
+                case "Pulgadas":
+                    outvalue = inval/2.54
+        case "Metros":
+            match inunit:
+                case "Kilometros":
+                    outvalue = inval/1000
+                case "Centimetros":
+                    outvalue = inval*100
+                case "Millas":
+                    outvalue = inval/1609
+                case "Pies":
+                    outvalue = inval*3.28
+                case "Pulgadas":
+                    outvalue = inval*39.37
+        case "Millas":
+            match inunit:
+                case "Kilometros":
+                    outvalue = inval*1.61
+                case "Centimetros":
+                    outvalue = inval*160900
+                case "Metros":
+                    outvalue = inval*1609
+                case "Pies":
+                    outvalue = inval*5280
+                case "Pulgadas":
+                    outvalue = inval*63360
+        case "Pies":
+            match inunit:
+                case "Kilometros":
+                    outvalue = inval*1.61
+                case "Centimetros":
+                    outvalue = inval*30.48
+                case "Metros":
+                    outvalue = inval/3.281
+                case "Millas":
+                    outvalue = inval/5280
+                case "Pulgadas":
+                    outvalue = inval*12
+        case "Pulgadas":
+            match inunit:
+                case "Kilometros":
+                    outvalue = inval/39370
+                case "Centimetros":
+                    outvalue = inval*2.54
+                case "Metros":
+                    outvalue = inval/39.37
+                case "Millas":
+                    outvalue = inval/63360
+                case "Pies":
+                    outvalue = inval/12
 
 print("Resultado:")
 if tipo_unit == "Temp":
-    print(str(inval) + "° " + inunit + " a " + str(round(outvalue, 2)) +"° " + outunit)
+    print(f"{inval}° {inunit} a {round(outvalue, 2)}° {outunit}")
+    #print(str(inval) + "° " + inunit + " a " + str(round(outvalue, 2)) +"° " + outunit)
+elif tipo_unit == "Dist":
+    print(f"{inval} {inunit} a {round(outvalue, 2)} {outunit}")
+    #print(str(inval)+ " " + inunit + " a " + str(round(outvalue, 2))+ " " + outunit)
